dcbc844f5d077390eb05df3b3b8739248a2f1cc2
DarkMastery <31713644+DarkMastery@users.noreply.github.com>
2017-11-05 23:25:40 +0800

    创建项目

---
+++ global.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace momoka_global {
+	static const float REFRESH_RATE = 60.F;
+	static const int TILE_SIZE = 60;
+	static const __int64 MULX = 1000000;
+};
+
---
+++ stdafx.h
@@ -0,0 +1,96 @@
+// stdafx.h : 标准系统包含文件的包含文件，
+// 或是经常使用但不常更改的
+// 特定于项目的包含文件
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN             // 从 Windows 头中排除极少使用的资料
+// Windows 头文件: 
+#include <windows.h>
+
+// C 运行时头文件
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+#include <math.h>
+
+
+// TODO:  在此处引用程序需要的其他头文件
+
+#include <d2d1.h>
+#include <d2d1helper.h>
+#include <dinput.h>
+#include <dwrite.h>
+#include <wincodec.h>
+
+#pragma comment(lib, "dinput8.lib")
+#pragma comment(lib, "d2d1.lib")
+#pragma comment(lib,"dxguid.lib")
+#pragma comment(lib,"dxgi.lib")
+#pragma comment(lib,"dwrite.lib")
+
+
+#ifndef HINST_THISCOMPONENT
+EXTERN_C IMAGE_DOS_HEADER __ImageBase;
+#define HINST_THISCOMPONENT ((HINSTANCE)&__ImageBase)
+#endif
+
+
+template <class Interface>
+inline void SafeRelease(
+	Interface** ppInterfaceToRelease
+) {
+	if (*ppInterfaceToRelease != NULL) {
+		(*ppInterfaceToRelease)->Release();
+
+		(*ppInterfaceToRelease) = NULL;
+	}
+}
+
+template <class Interface>
+inline void SafeUnacquire(
+	Interface** ppInterfaceToRelease
+) {
+	if (*ppInterfaceToRelease != NULL) {
+		(*ppInterfaceToRelease)->Unacquire();
+
+		(*ppInterfaceToRelease) = NULL;
+	}
+}
+
+template <class Interface>
+inline void SafeDelete(
+	Interface** ppInterfaceToRelease
+) {
+	if (*ppInterfaceToRelease != NULL) {
+		delete (*ppInterfaceToRelease);
+
+		(*ppInterfaceToRelease) = NULL;
+	}
+}
+
+inline LONGLONG GetCurrentTick() {
+	LARGE_INTEGER num;
+	QueryPerformanceCounter(&num);
+	return num.QuadPart;
+}
+
+inline LONGLONG GetCurrentFrequency() {
+	LARGE_INTEGER num;
+	QueryPerformanceFrequency(&num);
+	return num.QuadPart;
+}
+
+#ifndef Assert
+#if defined( DEBUG ) || defined( _DEBUG )
+#define Assert(b) do {if (!(b)) {OutputDebugStringA("Assert: " #b "\n");}} while(0)
+#else
+#define Assert(b)
+#endif //DEBUG || _DEBUG
+#endif
+
+#include "global.h"
