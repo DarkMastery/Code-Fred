f6db972b7a7257e7878dc85a93858c3b26da87d2
hanjinrui <641118318@qq.com>
2017-11-06 13:20:51 +0800

    Add files via upload

---
+++ fsm/hero/FallingState.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "fsm/HeroState.h"
+
+class FallingState : public HeroState {
+
+public:
+	explicit FallingState(Hero& hero);
+
+	~FallingState() override;
+	HeroState* Onland() override;
+	HeroState* Update() override;
+	HeroState* JumpKeyState(INPUT_KEY_EVENT keyEvent) override;
+};
