4924eae2ea780e7f54051b9c0bce0c89a56c9f4e
hanjinrui <641118318@qq.com>
2017-11-06 13:13:14 +0800

    Add files via upload

---
+++ include/fsm/HeroState.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "objects/Hero.h" 
+#include "services/InputService.h"
+class Hero;
+class HeroState {
+public:
+	explicit HeroState(Hero& hero);
+	virtual ~HeroState() = default;
+
+	void ClearFlag();
+
+	virtual HeroState* LeftKeyState(INPUT_KEY_EVENT keyEvent);
+	virtual HeroState* RightKeyState(INPUT_KEY_EVENT keyEvent);
+	virtual HeroState* JumpKeyState(INPUT_KEY_EVENT keyEvent);
+
+	virtual HeroState* Onland();
+	virtual HeroState* Update();
+protected:
+	Hero& m_hero_;
+
+	bool m_isOnland_;
+};
+
+
+
